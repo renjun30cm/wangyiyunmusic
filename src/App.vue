edaf93a44e8ab7de274044dc9f92d9febc87598e
zhang-sever <1909160864@qq.com>
2020-07-26 22:54:08 +0800

    “first”

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/> -->
+    <myHeader></myHeader>
+    <myFooter></myFooter>
+  </div>
+</template>
+<script>
+import myHeader from './components/myHeader'
+import myFooter from './components/myFooter'
+export default {
+  components:{
+    myHeader,
+    myFooter
+  }
+}
+</script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/components/myHeader.vue
@@ -0,0 +1,71 @@
+<template>
+  <header id="myHeader">
+    <div class="banxin">
+      <div class="menu">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div class="nav">
+        <a href>我的</a>
+        <a href>发现</a>
+        <a href>云村</a>
+        <a href>视频</a>
+      </div>
+      <div class="search iconfont icon-sousuo"></div>
+    </div>
+  </header>
+</template>
+<script>
+export default {
+    name:'myHeader',
+    data:function(){
+        return {
+            
+        }
+    }
+
+};
+</script>
+
+<style scoped>
+    a{
+      text-decoration: none;
+    }
+    header > div {
+    height: 46px;
+    display: flex;
+    overflow: hidden;
+    justify-content: space-between;
+    }
+    header .menu,
+    .search {
+    flex-basis: 68px;
+    /* width: 68px; */
+    line-height: 46px;
+    }
+    header .nav {
+    flex-grow: 1;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    }
+    header .nav > a {
+    /* font-weight: bold; */
+    font-family: 宋体;
+    }
+    header .menu > div {
+    width: 18px;
+    height: 2px;
+    background: #808183;
+    }
+    header .menu > div:first-of-type {
+    margin-top: 18px;
+    }
+    header .menu > div:last-of-type {
+    margin-bottom: 18px;
+    }
+    header .menu > div:nth-of-type(2) {
+    margin: 5px 0px;
+    }
+</style>
